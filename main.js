d78300d3e15e1e23ffa84584c30ca63f1d5d342e
dcasaponsa <dcasaponsa@gmail.com>
2021-09-30 13:40:30 +0200

    30092021/1340

---
+++ main.js
@@ -0,0 +1,11 @@
+var year2 = 2021;
+var month2 = 333220015;
+var decimales2 = 1.7 f;
+var nombre2 = "Tipo String";
+var visiesto2 = true;
+
+document.write(year2);
+document.write("Variable de tipo long " + month2);
+document.write("Variable de tipo long " + decimales2);
+document.write("Variable de tipo long " + nombre2);
+document.write("Variable de tipo long " + visiesto2);
