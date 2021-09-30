fef58686502e2d04da5f8cd63bfb2a85f93459b3
dcasaponsa <dcasaponsa@gmail.com>
2021-09-30 13:58:26 +0200

    30092021/13:58

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ var decimales2 = 1.7 f;
 var nombre2 = "Tipo String";
 var visiesto2 = true;
 
-document.write(year2);
-document.write("Variable de tipo long " + month2);
-document.write("Variable de tipo long " + decimales2);
-document.write("Variable de tipo long " + nombre2);
-document.write("Variable de tipo long " + visiesto2);
+var variables = document.getElementById("variables");
+variables.innerHTML = "Variable int " + year2;
+variables.innerHTML = "Variable long " + month2;
+variables.innerHTML = "Variable float " + decimales2;
+variables.innerHTML = "Variable String" + nombre2;
+variables.innerHTML = "Variable booleana" + visiesto2
