eecfe702d5a54ea976c01bb89d960e97e4fa2051
dcasaponsa <dcasaponsa@gmail.com>
2021-10-05 14:15:15 +0200

    05102021/14:15

--- Calculadora/Calculadora/MainWindow.xaml.cs
+++ Calculadora/Calculadora/MainWindow.xaml.cs
@@ -25,13 +25,23 @@ namespace Calculadora
             InitializeComponent();
         }
 
-        public int num1 { get; set; }
 
-        public bool AnadirNumero { get; set; }
+
+        public float num1 { get; set; }
+
+        public bool Sumar { get; set; }
+        public bool Restar { get; set; }
+        public bool Multi { get; set; }
+        public bool Dividir { get; set; }
+        public bool Raiz { get; set; }
+        
+
 
         private void btn_CE_Click(object sender, RoutedEventArgs e)
         {
             pantalla.Content = "";
+            pantallaOper.Content = "";
+            pantallaOperNum1.Content = "";
         }
 
         private void btn_1_Click(object sender, RoutedEventArgs e)
@@ -56,72 +66,144 @@ namespace Calculadora
 
         private void btn_5_Click(object sender, RoutedEventArgs e)
         {
-
+            pantalla.Content += "5";
         }
 
         private void btn_6_Click(object sender, RoutedEventArgs e)
         {
-
+            pantalla.Content += "6";
         }
 
         private void btn_7_Click(object sender, RoutedEventArgs e)
         {
-
+            pantalla.Content += "7";
         }
 
         private void btn_8_Click(object sender, RoutedEventArgs e)
         {
-
+            pantalla.Content += "8";
         }
 
         private void btn_9_Click(object sender, RoutedEventArgs e)
         {
+            pantalla.Content += "9";
+        }
 
+        private void btn_0_Click(object sender, RoutedEventArgs e)
+        {
+            pantalla.Content +="0";
+        }
+
+        private void btn_Decimal_Click(object sender, RoutedEventArgs e)
+        {
+            pantalla.Content += ".";
         }
 
         private void btn_suma_Click(object sender, RoutedEventArgs e)
         {
-            num1 = int.Parse(pantalla.Content.ToString());
+            pantallaOperNum1.Content = pantalla.Content;
+            num1 = float.Parse(pantalla.Content.ToString());            
             pantalla.Content = "";
-            AnadirNumero = true;
-            
-
+            pantallaOper.Content = "+";            
+            Sumar = true;
+ 
         }
 
         private void btn_resta_Click(object sender, RoutedEventArgs e)
         {
-
+            pantallaOperNum1.Content = pantalla.Content;
+            num1 = float.Parse(pantalla.Content.ToString());
+            pantalla.Content = "";
+            pantallaOper.Content = "-";
+            Restar = true;            
+         
         }
 
         private void btn_multi_Click(object sender, RoutedEventArgs e)
         {
-
+            pantallaOperNum1.Content = pantalla.Content;
+            num1 = float.Parse(pantalla.Content.ToString());
+            pantalla.Content = "";
+            pantallaOper.Content = "*";
+            Multi = true;
         }
 
         private void btn_divison_Click(object sender, RoutedEventArgs e)
         {
-
+            pantallaOperNum1.Content = pantalla.Content;
+            num1 = float.Parse(pantalla.Content.ToString());
+            pantalla.Content = "";
+            pantallaOper.Content = "/";
+            Dividir = true;
         }
 
         private void btn_sqrt_Click(object sender, RoutedEventArgs e)
         {
-
+            pantallaOperNum1.Content = pantalla.Content;
+            num1 = float.Parse(pantalla.Content.ToString());
+            pantalla.Content = "";
+            pantallaOper.Content = "sqrt";
+            Raiz = true;
+        
         }
 
         private void btn_Igual_Click(object sender, RoutedEventArgs e)
         {
-            var num2 = int.Parse(pantalla.Content.ToString());
 
-            if (AnadirNumero) {
-                var suma = num1 + num2;
+            var num2 = int.Parse(pantalla.Content.ToString());
+        
+            
+            if (Sumar)
+            {
+                float suma = num1 + num2;
                 pantalla.Content = suma.ToString();
             }
+
+            if (Restar)
+            {
+                float Resta = num1 - num2;
+                pantalla.Content = Resta.ToString();
+            }
+
+            if (Multi)
+            {
+                float Multiplica = num1 * num2;
+                pantalla.Content = Multiplica.ToString();
+            }
+
+            if (Dividir)
+            {
+                float Division = num1 / num2;
+                pantalla.Content = Division.ToString();
+            }
+
+            if (Raiz)
+            {
+               
+                double Cuadrada;
+                Cuadrada = (Math.Sqrt(num1));
+                pantalla.Content = Cuadrada.ToString();
+            }
+                     
         }
 
-        private void Suma() 
+
+        /*
+        private void Suma()  // Funcion Suma 
         {
-            var num1 = Int32.Parse(pantalla.Content.ToString());
+            float num1 = float.Parse(pantalla.Content.ToString()); // Cuando Pulsamos en la suma capturamos los numero 
+            
 
         }
+
+        private void Resta()
+        {
+            float num2 = float.Parse(pantalla.Content.ToString());
+
+        }*/
+
+
+
     }
+
 }
