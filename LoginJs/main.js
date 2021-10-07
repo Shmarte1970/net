3adcd3c77b20dd6a3926522ef1679ae74b1a6104
dcasaponsa <dcasaponsa@gmail.com>
2021-10-07 11:24:09 +0200

    7102021/11:24

---
+++ LoginJs/main.js
@@ -0,0 +1,20 @@
+let confirma;
+
+
+
+document.querySelector('#Login').addEventListener('click', () => {
+    Login = 'Login';
+
+
+    const valor1 = document.querySelector('#email').value;
+    const valor2 = document.querySelector('#pass').value;
+
+    if (valor1 == 'shmarte@gmail.com' && valor2 == '1234') {
+        document.querySelector('#confirma').innerHTML = "Datos Correctos";
+        // document.querySelector('#Tengo').innerHTML = valor1 + " " + valor2;
+    } else {
+        document.querySelector('#confirma').innerHTML = "Error en los datos";
+        // document.querySelector('#Tengo').innerHTML = valor1 + " " + valor2;
+    }
+
+});
